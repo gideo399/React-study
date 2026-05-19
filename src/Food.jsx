f33f699a97d000d616fb2fe966545a5724ed6d55
Gideon Tetteh Nyamedi <nyamedigideon1@gmail.com>
2026-05-19 09:34:37 +0000

    React component understood well

---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+function Food(){
+    
+    const food1 = "Waakye";
+    const food2 = "Orange";
+
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+
+
+    );
+}
+export default Food;
