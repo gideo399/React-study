744cc17b44860d3028e237936c17d4fd17e55850
Gideon Tetteh Nyamedi <nyamedigideon1@gmail.com>
2026-05-20 08:57:05 +0000

    More of props and good

---
+++ README.md
@@ -0,0 +1,6 @@
+Booleans are not usually appeared on the dom 
+ they are usually use with the ternary operator 
+
+
+ using prop types-- they help inn debugging purpose 
+ using default props that appear when no value is passed in the props 
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Student from './Student.jsx'
+function App() {
+  
+
+  return (
+
+  <>
+  <Student  name= "Kezia" age={30}  isStudent ={false} />
+  <Student  name= "Deon" age={35}  isStudent ={true} />
+  <Student  name= "Elorm" age={90}  isStudent ={false} />
+  
+  
+  </>
+
+  )
+}
+
+export default App
