5df8bd14d690cf3856d1ed0c9e0503c02fc7183c
Gideon Tetteh Nyamedi <nyamedigideon1@gmail.com>
2026-05-20 10:05:40 +0000

    Conditions and ternary operators

---
+++ conditions/src/App.jsx
@@ -0,0 +1,13 @@
+import UserGreetings from "./UserGreetings.jsx"
+
+function App() {
+
+
+  return (
+    <>
+      <UserGreetings  isLoggedIn={true} username="Deon"  />
+    </>
+  )
+}
+
+export default App
---
+++ conditions/src/UserGreetings.jsx
@@ -0,0 +1,8 @@
+function UserGreetings(props){
+
+   return( props.isLoggedIn ? <h2>Welcome {props.username} </h2> : <h2>Please log in to continue </h2>);
+        
+
+    }
+   
+export default UserGreetings;
